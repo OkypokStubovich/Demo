0a34ad47fa43b399a35509e10182d35bb4d7add8
Anna Romanenko <antony.takeshi@mail.ru>
2025-06-02 12:46:01 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -4,35 +4,14 @@
 
 using namespace std;
 
-// Функция для вычисления значения y(x)
 double calculateY(double x) {
-    double ln_x = log(x); // Вычисляем натуральный логарифм x
+    double ln_x = log(x);
     return sin(ln_x) - cos(ln_x) + 2 * ln_x;
-}
-
-// Функция для вывода числа с округлением до 4 знаков
-void printNumber(double num) {
-    // Округляем до 4 знаков после запятой
-    double rounded = round(num * 10000) / 10000;
-    
-    // Выводим целую часть
-    cout << static_cast<int>(rounded);
-    
-    // Выводим дробную часть, если она есть
-    double fractional = rounded - static_cast<int>(rounded);
-    if (abs(fractional) > numeric_limits<double>::epsilon()) {
-        cout << ".";
-        // Выводим 4 знака после запятой
-        fractional *= 10000;
-        int digits = static_cast<int>(round(fractional));
-        printf("%04d", digits);
-    }
 }
 
 int main() {
     double start, end, step;
     
-    // Ввод параметров с клавиатуры
     cout << "Введите начальное значение x: ";
     cin >> start;
     cout << "Введите конечное значение x: ";
@@ -40,34 +19,18 @@ int main() {
     cout << "Введите шаг табулирования: ";
     cin >> step;
     
-    // Проверка корректности шага
     if (step <= 0) {
-        cout << "Ошибка: шаг должен быть положительным" << endl;
+        cout << "Ошибка: шаг должен быть положительным\n";
         return 1;
     }
     
-    // Вывод заголовка таблицы
     cout << "\nТабулирование функции y = sin(ln x) - cos(ln x) + 2ln x\n";
-    cout << "на интервале [" << start << ", " << end << "] с шагом " << step << "\n\n";
-    cout << "     x          y(x)\n";
-    cout << "---------------------\n";
+    cout << "x\ty(x)\n";
+    cout << "----------------\n";
     
-    // Табулирование и вывод результатов
     for (double x = start; x <= end + numeric_limits<double>::epsilon(); x += step) {
-        // Проверка, что x положительный
-        if (x > 0) {
-            double y = calculateY(x);
-            
-            // Вывод x
-            cout << " ";
-            printNumber(x);
-            
-            // Вывод y(x)
-            cout << "     ";
-            printNumber(y);
-            cout << endl;
-        }
+        cout << x << "\t";
+        cout << (x > 0 ? to_string(calculateY(x)) : "не определено") << "\n";
     }
     
-    return 0;
-}
+    return
