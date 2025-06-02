50092b71ad5ce43e602c090edb860f9c905e2c4d
Anna Romanenko <antony.takeshi@mail.ru>
2025-06-02 10:42:41 +0300

    Update Романенко1.cpp

--- Романенко1.cpp
+++ Романенко1.cpp
@@ -1,14 +1,16 @@
 #include <iostream> 
 #include <cmath>
+#define_use_math_defines //for c++
 
 using namespace std;
 
 /**
 * @brief Функция для вычисления значения a по заданной формуле
 * @param x - значение переменной x
+* @param y - значение переменной у
 * @return возвращает рассчитанное значение a
 */
-double calculateA(double x);
+double calculateA(double x, double y);
 
 /**
 * @brief Функция для вычисления значения b по заданной формуле
@@ -24,14 +26,15 @@ int main() {
     const double y = 0.025;
     
     // Вычисление и вывод результатов
-    cout << "a = " << calculateA(x) << endl;
+    cout << "a = " << calculateA(x, y) << endl;
     cout << "b = " << calculateB(x, y) << endl;
     
     return 0;
 }
 
-double calculateA(double x) {
+double calculateA(double x, double y) {
     // Вычисляем a = 1 + x + x²/2 + x³/3 + x⁴/4
+    (void)y;
     return 1 + x + (x*x)/2 + (x*x*x)/3 + (x*x*x*x)/4;
 }
 
